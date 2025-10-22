3728547d7ddfcd9d161e1d8c6cd711a734889360
HunterYarmy <huntery5@nycstudents.net>
2025-10-22 15:33:41 -0400

    added read and text file as well as fixing test cases

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,27 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 public class Main {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook a = new AppointmentBook(schedule);
 
-        for(int i = 10; i < 15; i++) schedule[1][i] = true;
+        for (int i = 10; i < 15; i++) schedule[1][i] = true;
+        for (int i = 30; i < 45; i++) schedule[1][i] = true;
+        for (int i = 50; i < 60; i++) schedule[1][i] = true;
         a.printPeriod(2);
-        System.out.println(a.findFreeBlock(2,15));
+        System.out.println(a.findFreeBlock(2, 15));
+        System.out.println(a.findFreeBlock(2, 9));
+        System.out.println(a.findFreeBlock(2, 20));
 
         schedule = new boolean[8][60];
 
-        for(int i = 25; i < 30; i++) schedule[1][i] = true;
-        for(int i = 0; i < 15; i++) schedule[2][i] = true;
-        for(int i = 41; i < 60; i++) schedule[2][i] = true;
-        for(int i = 5; i < 30; i++) schedule[3][i] = true;
-        for(int i = 44; i < 60; i++) schedule[3][i] = true;
+        setSchedule(schedule);
 
         AppointmentBook b = new AppointmentBook(schedule);
 
         int period = 2;
-        while(period < 5) {
+        while (period < 5) {
             System.out.println("Period: " + period);
             b.printPeriod(period);
             period++;
@@ -27,5 +30,36 @@ public class Main {
         b.printPeriod(4);
         System.out.println(b.makeAppointment(2, 4, 22));
         b.printPeriod(4);
+        setSchedule(schedule);
+        System.out.println(b.makeAppointment(3, 4, 3));
+        for (int i = 25; i < 30; i++) schedule[1][i] = true;
+
+        setSchedule(schedule);
+        System.out.println(b.makeAppointment(2, 4, 30));
+
+
     }
+
+    public static void setSchedule(boolean[][] schedule) {
+        for (int i = 25; i < 30; i++) schedule[1][i] = true;
+        for (int i = 0; i < 15; i++) schedule[2][i] = true;
+        for (int i = 41; i < 60; i++) schedule[2][i] = true;
+        for (int i = 5; i < 30; i++) schedule[3][i] = true;
+        for (int i = 44; i < 60; i++) schedule[3][i] = true;
+    }
+
+
+    public static int read() throws FileNotFoundException {
+        int fulfilled = 0;
+
+        File f = new File("Schedules.txt");
+        Scanner s = new Scanner(f);
+
+        Boolean[][] schedule = new Boolean[8][60];
+
+        for (int i = 0; i < 60; i++) {
+
+        }
+}
+
 }
